f90b531d3677d012b040a4f49e239d0388bd86db
hbisetto <hbisetto@yahoo.com.br>
2023-12-20 15:46:48 -0300

    Erro: LBYL + race condition

--- errors.py
+++ errors.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 
-names = open("names.txt").readlines()
+import os
+import sys
 
-print(names[2])
+if os.path.exists("names.txt"):
+    print("The file exists")
+    input("...") # Race Condition
+    names = open("names.txt").readlines()
+else:  
+    print("[Error] Files names.txt not found")
+    sys.exit(1)
+
+# LBYL - Look Before You Leap
+
+if len(names) >= 3:
+    print(names[2])
+else:
+    print("[Error]: Missing name in the list")
+    sys.exit(1)
+    
