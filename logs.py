b9f25183bca6aa4b17f43331d08a5a1d81d65a54
hbisetto <hbisetto@yahoo.com.br>
2024-03-26 15:59:29 -0300

    using fileHandler

--- logs.py
+++ logs.py
@@ -2,6 +2,7 @@
 
 import os
 import logging
+from logging import handlers
 
 # TODO: usar função
 # TODO: usar lib (loguru)
@@ -10,17 +11,23 @@ log_level = os.getenv("LOG_LEVEL", "WARNING").upper()
 
 # nossa instância de log
 log = logging.Logger("logs.py", log_level)
- 
 # level
-ch = logging.StreamHandler() # Console/terminal/stderr
-ch.setLevel(log_level)
+# ch = logging.StreamHandler() # Console/terminal/stderr
+# ch.setLevel(log_level)
+fh = handlers.RotatingFileHandler(
+    "meulog.log",
+    maxBytes=10**6,  # costuma-se usar maxBytes=10**6 , que é igual a 1MB
+    backupCount=10
+)
+fh.setLevel(log.level)
 # formatação
 fmt = logging.Formatter(
     '%(asctime)s  %(name)s  %(levelname)s l:%(lineno)d f:%(filename)s: %(message)s'
 )
-ch.setFormatter(fmt)
+# ch.setFormatter(fmt)
+fh.setFormatter(fmt)
 # destino
-log.addHandler(ch)
+log.addHandler(fh)
 
 
 # logging  root logger
@@ -31,7 +38,7 @@ log.warning("Aviso que não causa erro")
 log.error("Erro que não afeta uma única execução")
 log.critical("Erro geral ex: o banco de dados sumiu")
 """
-print("-" * 30)
+
 
 try: 
     1 / 0
